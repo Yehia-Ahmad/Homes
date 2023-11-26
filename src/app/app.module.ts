bd30271cae3c7f8f9f1c331ab53902959aa38ded
Yehia <Yehia>
2023-11-26 13:48:11 +0200

    UPDATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HousingLocationComponent } from './Components/housing-location/housing-location.component';
+import { DetailsComponent } from './Components/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HousingLocationComponent],
+  declarations: [AppComponent, HomeComponent, HousingLocationComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
