6aad49946f40791eeaa81cf045c1b011ce0fed1a
Yehia <Yehia>
2023-11-26 14:28:38 +0200

    UPDATE

--- src/app/Components/details/details.component.ts
+++ src/app/Components/details/details.component.ts
@@ -2,6 +2,7 @@ import { HousingService } from 'src/app/Services/housing.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Housinglocation } from 'src/app/Models/housinglocation';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -11,6 +12,12 @@ import { Housinglocation } from 'src/app/Models/housinglocation';
 export class DetailsComponent implements OnInit {
   id: number;
   housingLocation: Housinglocation | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+  });
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,5 +29,15 @@ export class DetailsComponent implements OnInit {
     //Add 'implements OnInit' to the class.
     this.id = Number(this.activatedRoute.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.id);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+
+    this.applyForm.reset();
   }
 }
